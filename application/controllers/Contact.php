32be511381af94c22cfd1cf6f5dda49fe3296e20
asimbilaladil <asimbilal@mail.com>
2020-02-04 22:17:09 +0100

    Update Contact.php

--- application/controllers/Contact.php
+++ application/controllers/Contact.php
@@ -9,4 +9,19 @@ class Contact extends CI_Controller {
       $this->load->view('contact');
       $this->load->view('footer');
 	}
+	
+	public function email() {
+		$subject = $this->input->post('subject');
+		$message = $this->input->post('message');
+		$email = $this->input->post('email');
+		$fname = $this->input->post('fname');
+		$lname = $this->input->post('lname');
+		
+		$this->load->library('email');
+		$this->email->from($email, $fname . ' ' . $lname);
+		$this->email->to('m.moazzam12@gmail.com');
+		$this->email->subject($subject);
+		$this->email->message($message);
+		$this->email->send();
+	}
 }
